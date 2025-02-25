ea83935db91c55d8d694e80c5ff85ecab3e7cbfb
Judah Lomo <judahllomo@gmail.com>
2025-02-24 20:38:51 -0600

    Initial Commit

---
+++ MemoryGameApp/MemoryGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameAppApp.swift
+//  MemoryGameApp
+//
+//  Created by Judah Lomo on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGameAppTests/MemoryGameAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameAppTests.swift
+//  MemoryGameAppTests
+//
+//  Created by Judah Lomo on 2/24/25.
+//
+
+import Testing
+@testable import MemoryGameApp
+
+struct MemoryGameAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
